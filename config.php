9a469629e06c7941f8ae8bcec1c1dd12a8973d75
MaliqueMaju <mmalikibrahim007@gmail.com>
2025-08-03 20:31:49 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+
+if(!defined('BASEPATH')) echo "Tidak bisa langsung mengakses halaman ini!";
+
+define('DEFAULT_CONTROLLER', 'C_Auth');
+define('BASE_URL', 'http://localhost/rental_mobil_malik/');
+define('APP_NAME', 'Malik\'s Rental Mobil');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'rental_mobil_malik');
+
+?>
